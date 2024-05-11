ce862803d8f9ff7c63dddd450adc5dd252c13771
DarkMatter-999 <darkmatter999official@gmail.com>
2024-05-11 22:27:58 +0530

    Added initial Admin section

---
+++ admin/index.php
@@ -0,0 +1,67 @@
+<?php 
+
+include "../header.php";
+
+?>
+
+<body>
+<?php
+
+include "../nav.php";
+
+?>
+<div class="container mt-5">
+         <div class="container text-center">
+             <div class="row row-cols-5">
+               <a href="teacher.php" 
+                  class="col btn btn-dark m-2 py-3">
+                 <i class="fa fa-user-md fs-1" aria-hidden="true"></i><br>
+                  Teachers
+               </a> 
+               <a href="student.php" class="col btn btn-dark m-2 py-3">
+                 <i class="fa fa-graduation-cap fs-1" aria-hidden="true"></i><br>
+                  Students
+               </a> 
+               <a href="registrar-office.php" class="col btn btn-dark m-2 py-3">
+                 <i class="fa fa-pencil-square fs-1" aria-hidden="true"></i><br>
+                  Registrar Office
+               </a> 
+               <a href="class.php" class="col btn btn-dark m-2 py-3">
+                 <i class="fa fa-cubes fs-1" aria-hidden="true"></i><br>
+                  Class
+               </a> 
+               <a href="section.php" class="col btn btn-dark m-2 py-3">
+                 <i class="fa fa-columns fs-1" aria-hidden="true"></i><br>
+                  Section
+               </a> 
+               <a href="grade.php" class="col btn btn-dark m-2 py-3">
+                 <i class="fa fa-level-up fs-1" aria-hidden="true"></i><br>
+                  Grade
+               </a> 
+               <a href="course.php" class="col btn btn-dark m-2 py-3">
+                 <i class="fa fa-book fs-1" aria-hidden="true"></i><br>
+                  Course
+               </a> 
+               <a href="message.php" class="col btn btn-dark m-2 py-3">
+                 <i class="fa fa-envelope fs-1" aria-hidden="true"></i><br>
+                  Message
+               </a> 
+               <a href="settings.php" class="col btn btn-primary m-2 py-3 col-5">
+                 <i class="fa fa-cogs fs-1" aria-hidden="true"></i><br>
+                  Settings
+               </a> 
+               <a href="../logout.php" class="col btn btn-warning m-2 py-3 col-5">
+                 <i class="fa fa-sign-out fs-1" aria-hidden="true"></i><br>
+                  Logout
+               </a> 
+             </div>
+         </div>
+     </div>
+
+</body>
+
+<?php
+
+include "../footer.php";
+
+?>
